3feacaf61f5e4187cdd73dad528a6e2107777750
Anteneh2323 <145783826+Anteneh2323@users.noreply.github.com>
2024-11-06 09:25:22 -0600

    Final commit: Deploy frontend, backend, and admin panel to production

--- frontEnd/src/context/StoreContext.jsx
+++ frontEnd/src/context/StoreContext.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { createContext, useEffect, useState, useRef } from "react";
-import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
